f1cbe9aa537edccd7b000abae742d071eb3c433e
matech <matech@sch.bme.hu>
2018-05-16 14:50:48 +0200

    uses tf.estimator

--- main.py
+++ main.py
@@ -1,89 +1,98 @@
+import shutil
+
+import numpy as np
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
 
 print(tf.__version__)
 
 tensorboard_dir = "tensorboard/"
 
 
-def fc_layer(input, input_size, output_size):
-    with tf.name_scope('fully_connected') as scope:
-        W_fc1 = weight_variable([input_size, output_size])
-        b_fc1 = bias_variable([output_size])
-        return tf.nn.relu(tf.matmul(input, W_fc1) + b_fc1)
+def fc_layer(input_layer, output_size):
+    return tf.layers.dense(inputs=input_layer, units=output_size, activation=tf.nn.relu)
 
 
-def conv_layer(input, kernel_size, input_ch, output_ch, stride=1):
-    with tf.name_scope('convolutional') as scope:
-        W_conv1 = weight_variable(
-            [kernel_size, kernel_size, input_ch, output_ch])  # with, height, channel, output_channels
-        b_conv1 = bias_variable([output_ch])
-        return tf.nn.relu(conv2d(input, W_conv1, stride) + b_conv1)
+def conv_layer(input_layer, output_ch, strides=1, kernel_size=5):
+    return tf.layers.conv2d(
+        inputs=input_layer,
+        filters=output_ch,
+        kernel_size=[kernel_size, kernel_size],
+        strides=(strides, strides),
+        padding="same",
+        activation=tf.nn.relu)
 
 
-def dw_conv_layer(input, kernel_size, input_ch, output_ch, stride=1):
-    W_conv1 = weight_variable([kernel_size, kernel_size, input_ch, 1])  # with, height, channel, output_channels
-    W_conv2 = weight_variable([1, 1, input_ch, output_ch])  # with, height, channel, output_channels
-    b_conv2 = bias_variable([output_ch])
-    return tf.nn.relu(tf.nn.separable_conv2d(input, W_conv1, W_conv2, [1, stride, stride, 1], 'SAME') + b_conv2)
+def dropout(input_layer, mode, rate=0.4):
+    return tf.layers.dropout(inputs=input_layer, rate=rate, training=mode == tf.estimator.ModeKeys.TRAIN)
 
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+def max_pool_2x2(input_layer, pool_size=2):
+    return tf.layers.max_pooling2d(inputs=input_layer, pool_size=[pool_size, pool_size], strides=pool_size)
 
 
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-
-def conv2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1], padding='SAME')
+def cnn_model_fn(features, labels, mode):
+    input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+    conv1 = conv_layer(input_layer, 32, 2)  # 14
+    conv2 = conv_layer(conv1, 32, 1)  # 14
+    conv3 = conv_layer(conv2, 64, 2)  # 7
+    conv4 = conv_layer(conv3, 64, 1)  # 7
+    flat_size = 7 * 7 * 64
+    flat = tf.reshape(conv4, [-1, flat_size])
+    fc = fc_layer(flat, 1024)
+    droped = dropout(fc, mode)
+    logits = fc_layer(droped, 10)
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=logits, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.AdamOptimizer()
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"])}
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
 def main():
-    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
-    x = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
-    y_ = tf.placeholder(tf.float32, shape=[None, 10])
-    keep_prob = tf.placeholder(tf.float32)
-    conv1 = conv_layer(x, 3, 1, 32, 2)  # 14
-    conv2 = conv_layer(conv1, 3, 32, 32, 1)  # 14
-    conv3 = conv_layer(conv2, 3, 32, 64, 2)  # 7
-    conv4 = conv_layer(conv3, 3, 64, 64, 1)  # 7
-    flat_size = 7 * 7 * 64
-    flat = tf.reshape(conv4, [-1, flat_size])
-    fc = fc_layer(flat, flat_size, 1024)
-    droped = tf.nn.dropout(fc, keep_prob)
-    y = fc_layer(droped, 1024, 10)
-    with tf.name_scope('loss_calculation') as scope:
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    with tf.name_scope('check_accuracy') as scope:
-        correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    writer = tf.summary.FileWriter(tensorboard_dir)
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for i in range(10000):
-            batch = mnist.train.next_batch(1000)
-            images = batch[0].reshape((-1, 28, 28, 1))
-            if i % 100 == 0:
-                train_accuracy = accuracy.eval(feed_dict={x: images, y_: batch[1], keep_prob: 1.0})
-                print('step %d, training accuracy %g' % (i, train_accuracy))
-            train_step.run(feed_dict={x: images, y_: batch[1], keep_prob: 0.5})
-        print('test accuracy %g' % accuracy.eval(feed_dict={
-            x: mnist.test.images.reshape((-1, 28, 28, 1)), y_: mnist.test.labels, keep_prob: 1.0}))
-
-    writer.add_graph(sess.graph)
-    writer.close()
+    model_dir = "mnist_convnet_model/"
+    shutil.rmtree(model_dir )
+    print("Tensorflow version: " + tf.__version__)
+    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+    train_data = mnist.train.images  # Returns np.array
+    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+    eval_data = mnist.test.images  # Returns np.array
+    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+    mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir=model_dir)
+    tensors_to_log = {"probabilities": "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=100)
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": train_data},
+        y=train_labels,
+        batch_size=1000,
+        num_epochs=None,
+        shuffle=True)
+    mnist_classifier.train(
+        input_fn=train_input_fn,
+        steps=20000,
+        hooks=[logging_hook])
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False)
+    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+    print(eval_results)
 
 
 if __name__ == "__main__":
